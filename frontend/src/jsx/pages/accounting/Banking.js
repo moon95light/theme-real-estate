024267df0d091341426961e4761df880ed6d3c80
antonius95 <antonio.torres.tech@gmail.com>
2023-08-15 14:09:35 +0200

    add banking
    add banking

--- frontend/src/jsx/pages/accounting/Banking.js
+++ frontend/src/jsx/pages/accounting/Banking.js
@@ -1,15 +1,117 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PageTitle from "../../layouts/PageTitle";
+import {
+    Dropdown, ProgressBar, ButtonGroup, Button,
+    Card,
+    SplitButton,
+    DropdownButton,
+} from "react-bootstrap";
+import '../../../css/financial.css';
+import { useTranslation } from 'react-i18next';
 
 
-function AnAccountingBanking() {
-    return (
-        <>
-            <div className="form-head page-titles d-flex  align-items-center">
-            AnAccountingBanking            
-            </div>
+/// images
+import { Link } from "react-router-dom";
 
-        </>
+const AnAccountingBanking = () => {
+    const { t } = useTranslation();
+    const drop = (
+        <Dropdown>
+            <Dropdown.Toggle variant="primary" className="table-dropdown i-false btn  tp-btn-light sharp">
+                <span className="fs--1">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 24 24" version="1.1">
+                        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                            <rect x="0" y="0" width="24" height="24"></rect>
+                            <circle fill="#000000" cx="5" cy="12" r="2"></circle>
+                            <circle fill="#000000" cx="12" cy="12" r="2"></circle>
+                            <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+                        </g>
+                    </svg>
+                </span>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item href="#">{t('Applicant summary')}</Dropdown.Item>
+                <Dropdown.Item href="#">{t('View application')}</Dropdown.Item>
+                <Dropdown.Item href="#">{t('Run credit check')}</Dropdown.Item>
+                <Dropdown.Item href="#">{t('Add lease')}</Dropdown.Item>
+                <Dropdown.Item href="#">{t('Send opt-in text message')}</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
     );
-}
+
+    return (
+        <Fragment>
+            <div className="page-titles">
+                <h4 className="page-title">{t('Banking')}</h4>
+                <div className="property-buttons">
+                    <button type="button" className="mr-2 btn btn-xs btn-success" id="btn-success">{t('Add bank account')}</button>
+                    <Button className='mr-2 btn-xs' variant='outline-dark'>{t('Print check')}</Button>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12">
+                    <div className="card">
+                        <Card.Header>
+                            <Dropdown>
+                                <Dropdown.Toggle variant='' className='pl-0 mt-1 mb-2'>
+                                    Add filter option
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href='#'>{t('Status')}</Dropdown.Item>
+                                    <Dropdown.Item href='#'>{t('Name')}</Dropdown.Item>
+                                    <Dropdown.Item href='#'>{t('Account number')}</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Card.Header>
+                        <div className="card-body">
+                            <div className="table-responsive">
+                                <table className="table mb-0 table-striped respon-table-data">
+                                    <thead>
+                                        <tr>
+                                            <th>{t('NAME')}</th>
+                                            <th>{t('ACCOUNT NUMBER')}</th>
+                                            <th>{t('EPAY ENABLED')}</th>
+                                            <th>{t('RETAIL')} <br></br> {t('CASH ENABLED')}</th>
+                                            <th>{t('LAST RECONCILIATION')} <br></br>{t('DATE')}</th>
+                                            <th>{t('UNDEPOSITED')} <br></br> {t('FUNDS')}</th>
+                                            <th>{t('BALANCE')}</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="customers">
+                                        <tr className="btn-reveal-trigger">
+                                            <td>
+                                                <div>
+                                                    <a className="banking-name-up" href="/">Company checking</a>
+                                                </div>
+                                                <div className="banking-name-down">
+                                                    <p href="/">Company operating funds</p>
+                                                </div>
+                                            </td>
+                                            <td className="py-2">
+                                                ****2345
+                                            </td>
+                                            <td className="py-2">
+                                                
+                                            </td>
+                                            <td className="py-2">
+                                                
+                                            </td>
+                                            <td className="py-2">30/03/2018</td>
+                                            <td>
+                                            </td>
+                                            <td className="py-2" style={{display: 'inline-flex'}}>
+                                                <span style={{paddingTop: '10px'}}>$4,9000</span><span> {drop}</span></td>
+                                        </tr>
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
 
 export default AnAccountingBanking;
--- frontend/src/jsx/pages/accounting/Bills.js
+++ frontend/src/jsx/pages/accounting/Bills.js
@@ -1,15 +1,277 @@
-import React from "react";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import PageTitle from '../../layouts/PageTitle';
+import { useTranslation } from 'react-i18next';
 
+import {
+    Row, Col, Table, Card, Tab, Nav, Badge, Button,
+    SplitButton, Dropdown, DropdownButton, ButtonGroup
+} from 'react-bootstrap'
+import '../../../css/financial.css';
 
-function AnAccountingBills() {
-    return (
-        <>
-            <div className="form-head page-titles d-flex  align-items-center">
-            AnAccountingBills            
-            </div>
-
-        </>
+const AnAccountingBills = () => {
+    const { t } = useTranslation();
+    const svg1 = (
+        <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
+            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <rect x="0" y="0" width="24" height="24"></rect>
+                <circle fill="#000000" cx="5" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="12" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+            </g>
+        </svg>
     );
+    const tabData = [
+        {
+            name: 'Home',
+            title: t('Bills'),
+            content: (
+                <Col lg={12}>
+                    <Card>
+                        <Card.Title>
+                            <div className="property-buttons">
+                                <button type="button" className="mr-2 btn btn-xs btn-success" id="btn-success">{t('Add')} {t('properties')}</button>
+
+                                <DropdownButton
+                                    as={ButtonGroup}
+                                    id='dropdown-button-drop-down'
+                                    drop='down'
+                                    variant='outline-info'
+                                    size='xs'
+                                    className='mr-2 outline'
+                                    title=' Managemnet fees'
+                                >
+                                    <Dropdown.Item href='#'>{t('Collect')} {t('Management')} {t('fees')}</Dropdown.Item>
+                                    <Dropdown.Item href='#'>{t('Pay')} {t('out')} {t('income')} {t('management')} {t('accounts')}</Dropdown.Item>
+                                </DropdownButton>
+                                <Button className='mr-2 btn-xs' variant='outline-info'>
+                                    {t('Update')} {t('unit')} {t('details')}
+                                </Button>
+                                <Button className='mr-2 btn-xs' variant='outline-info'>
+                                    {t('Property')} {t('groups')}
+                                </Button>
+                            </div>
+                        </Card.Title>
+                        <Card.Header className='listing-card-header'>
+                            <div className='input-group col-sm-2 input-group-sm'>
+                                <input type='text' className='form-control' placeholder={t('All') + ' ' + t('rentals')} />
+                                <Dropdown className='input-group-prepend'>
+                                    <Dropdown.Toggle
+                                        variant=''
+                                        className='btn btn-outline dropdown-toggle'
+                                        type='button'
+                                        data-toggle='dropdown'
+                                    >
+
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu className='dropdown-menu'>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Action
+                                        </Dropdown.Item>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Another action
+                                        </Dropdown.Item>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Something else here
+                                        </Dropdown.Item>
+                                        <div
+                                            role='separator'
+                                            className='dropdown-divider'
+                                        ></div>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Separated link
+                                        </Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>{' '}
+                            </div>
+                            <Dropdown>
+                                <Dropdown.Toggle variant='' className='pl-0 mt-1 mb-2'>
+                                    Add filter option
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href='#'>Unit</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Card.Header>
+                        <Card.Body>
+                            <Table responsive bordered>
+                                <thead>
+                                    <tr>
+                                        <th>{t('LISTED')}</th>
+                                        <th>{t('AVAILABLE')}</th>
+                                        <th>{t('UNIT')}</th>
+                                        <th>{t('BEDS')}</th>
+                                        <th>{t('BATHS')}</th>
+                                        <th>{t('SIZE')}</th>
+                                        <th className='remove-listing-rent'>{t('LISTING RENT')}</th>
+                                        <th className='remove-th'></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><Link to="/" className='listing-listed'>7/1/2022</Link></td>
+                                        <td>7/1/2023</td>
+                                        <td>Garden Row (multi building complex)-2D</td>
+                                        <td>1 Bed</td>
+                                        <td>2 Bath</td>
+                                        <td>700</td>
+                                        <td className='listing-rent'>
+                                            $ 599
+                                        </td>
+                                        <td className="remove-td">
+                                            <Dropdown>
+                                                <Dropdown.Toggle
+                                                    variant="success"
+                                                    className="light sharp i-false"
+                                                >
+                                                    {svg1}
+                                                </Dropdown.Toggle>
+                                                <Dropdown.Menu>
+                                                    <Dropdown.Item>Post to craigslist</Dropdown.Item>
+                                                    <Dropdown.Item>Export to html</Dropdown.Item>
+                                                    <Dropdown.Item>Dlist unit</Dropdown.Item>
+                                                    <Dropdown.Item>Edit listing</Dropdown.Item>
+                                                </Dropdown.Menu>
+                                            </Dropdown>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ),
+        },
+        {
+            name: 'Profile',
+            title: t('Unlisted units'),
+            content: (
+                <Col lg={12}>
+                    <Card>
+                        <Card.Header className='listing-card-header'>
+                            <div className='input-group col-sm-2 input-group-sm'>
+                                <input type='text' className='form-control' placeholder={t('All') + ' ' + t('rentals')} />
+                                <Dropdown className='input-group-prepend'>
+                                    <Dropdown.Toggle
+                                        variant=''
+                                        className='btn btn-outline dropdown-toggle'
+                                        type='button'
+                                        data-toggle='dropdown'
+                                    >
+
+                                    </Dropdown.Toggle>
+                                    <Dropdown.Menu className='dropdown-menu'>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Action
+                                        </Dropdown.Item>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Another action
+                                        </Dropdown.Item>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Something else here
+                                        </Dropdown.Item>
+                                        <div
+                                            role='separator'
+                                            className='dropdown-divider'
+                                        ></div>
+                                        <Dropdown.Item className='dropdown-item' to='#'>
+                                            Separated link
+                                        </Dropdown.Item>
+                                    </Dropdown.Menu>
+                                </Dropdown>{' '}
+                            </div>
+                            <Dropdown>
+                                <Dropdown.Toggle variant='' className='pl-0 mt-1 mb-2'>
+                                    Add filter option
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu>
+                                    <Dropdown.Item href='#'>Unit</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </Card.Header>
+                        <Card.Body>
+                            <Table responsive bordered>
+                                <thead>
+                                    <tr>
+                                        <th>{t('STATUS')}</th>
+                                        <th>{t('LEASE END')}</th>
+                                        <th>{t('NEXT LEASE')}</th>
+                                        <th>{t('UNIT')}</th>
+                                        <th>{t('TENANTS')}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td><Link to="/" className='listing-listed'>List unit</Link></td>
+                                        <td>7/1/2023</td>
+                                        <td>--</td>
+                                        <td>Garden Row (multi building complex)-2D</td>
+                                        <td>Clifton Munoz, Virginia Gonzalez</td>
+                                    </tr>
+                                    <tr>
+                                        <td><Link to="/" className='listing-listed'>List unit</Link></td>
+                                        <td>7/1/2023</td>
+                                        <td>--</td>
+                                        <td>Garden Row (multi building complex)-2D</td>
+                                        <td>Clifton Munoz, Virginia Gonzalez</td>
+                                    </tr>
+                                    <tr>
+                                        <td><Link to="/" className='listing-listed'>List unit</Link></td>
+                                        <td>7/1/2023</td>
+                                        <td>--</td>
+                                        <td>Garden Row (multi building complex)-2D</td>
+                                        <td>Clifton Munoz, Virginia Gonzalez</td>
+                                    </tr>
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            ),
+        },
+
+    ]
+
+    return (
+        <Fragment>
+            <div className="page-titles">
+                <h4 className="page-title">  {t('Bills')}</h4>
+            </div>
+            <Row>
+                <div className='col-xl-12'>
+                    <Card>
+
+                        <Card.Body>
+                            <Tab.Container defaultActiveKey={tabData[0].name.toLowerCase()}>
+                                <Nav as='ul' className='nav-tabs'>
+                                    {tabData.map(
+                                        (data, i) =>
+                                            i !== tabData.length && (
+                                                <Nav.Item as='li' key={i}>
+                                                    <Nav.Link eventKey={data.name.toLowerCase()}>
+                                                        {data.title}
+                                                    </Nav.Link>
+                                                </Nav.Item>
+                                            )
+                                    )}
+                                </Nav>
+                                <Tab.Content className='pt-4'>
+                                    {tabData.map(
+                                        (data, i) =>
+                                            i !== tabData.length && (
+                                                <Tab.Pane eventKey={data.name.toLowerCase()} key={i}>
+                                                    <p>{data.content}</p>
+                                                </Tab.Pane>
+                                            )
+                                    )}
+                                </Tab.Content>
+                            </Tab.Container>
+                        </Card.Body>
+                    </Card>
+                </div>
+            </Row>
+        </Fragment>
+    )
 }
 
 export default AnAccountingBills;
