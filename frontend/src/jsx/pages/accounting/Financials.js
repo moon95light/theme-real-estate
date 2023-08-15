11503686a85bc21ad1455bb01eb01a6f9e68233a
antonius95 <antonio.torres.tech@gmail.com>
2023-08-15 06:45:05 +0200

    Financial & GeneralLedger
    Financial & GeneralLedger

--- frontend/src/jsx/pages/accounting/Financials.js
+++ frontend/src/jsx/pages/accounting/Financials.js
@@ -1,15 +1,169 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PageTitle from "../../layouts/PageTitle";
+import { useState, useEffect } from "react";
+import Select from 'react-select';
+import { useTranslation } from 'react-i18next';
+import {
+    Row,
+    Col,
+    Card,
+    Table,
+    Button,
+    Dropdown,
+    DropdownButton,
+    ButtonGroup,
+    SplitButton,
+} from "react-bootstrap";
+
+import '../../../css/financial.css';
+
+import { Link } from "react-router-dom";
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
+
+const AnAccountingFinancials = () => {
+    const { t } = useTranslation();
+    const svg1 = (
+        <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
+            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <rect x="0" y="0" width="24" height="24"></rect>
+                <circle fill="#000000" cx="5" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="12" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+            </g>
+        </svg>
+    );
+    // Normal page
+    /*  const [data, setData] = useState(
+       document.querySelectorAll(".custom-checkbox")
+     ); */
+    const [test, settest] = useState(false);
+    // use effect
+    useEffect(() => {
+        //   getAppData();
+        //setData(document.querySelectorAll(".custom-checkbox"));
+        setTimeout(() => {
+            settest(true);
+        }, 2000);
+    }, [test]);
 
 
-function AnAccountingFinancials() {
     return (
         <>
-            <div className="form-head page-titles d-flex  align-items-center">
-            AnAccountingFinancials            
-            </div>
+            <Fragment>
+                <div className="page-titles">
+                    <h4 className="page-title">{t('Financials')}</h4>
 
+                </div>
+                <Row>
+                    <Col lg={12}>
+                        <Card>
+                            <Card.Header>
+                                <div>
+                                    <p className="company-and-property">{t('PROPERTY OR COMPANY')}</p>
+                                    <SplitButton
+                                        as={ButtonGroup}
+                                        id='dropdown-button-drop-down'
+                                        drop='down'
+                                        variant='outline-primary'
+                                        size='sm'
+                                        title={t('All properties')}
+                                        className='mt-1 mr-4'
+                                    >
+                                        <Dropdown.Item href='#'>{t('All')} {t('rentals')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>74 Grove Street (Single family home)</Dropdown.Item>
+                                        <Dropdown.Item href='#'>160 East End Avenue (condo/townhouse)</Dropdown.Item>
+                                        <div className='dropdown-divider'></div>
+                                        <Dropdown.Item href='#'>Separated link</Dropdown.Item>
+                                    </SplitButton>
+                                </div>
+                                <div>
+                                    <p className="company-and-property">{t('PERIOD')}</p>
+                                    <DropdownButton
+                                        as={ButtonGroup}
+                                        id='dropdown-button-drop-down'
+                                        drop='down'
+                                        variant='outline-primary'
+                                        size='sm'
+                                        className='mt-1 mr-2'
+                                        title={t('Year to date')}
+                                    >
+                                        <Dropdown.Item href='#'>{t('Year to date')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>{t('Last month')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>{t('Last quarter')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>{t('Last year')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>{t('This year vs. last year')}</Dropdown.Item>
+                                        <Dropdown.Item href='#'>{t('Last year vs. prior year')}</Dropdown.Item>
+                                    </DropdownButton>
+                                </div>
+                                <button type="button" className="mr-2 ml-3 btn btn-sm btn-success mt-4" id="btn-success">{t('Search')}</button>
+                                <div className='card-body mt-2 financial-radio'>
+                                    <div className='basic-form'>
+                                        <form onSubmit={(e) => e.preventDefault()}>
+                                            <div className='form-group mb-0'>
+                                                <label className='radio-inline mr-3'>
+                                                    <input type='radio' name='optradio' checked /> {t('Cash basis')}
+                                                </label>
+                                                <label className='radio-inline mr-3'>
+                                                    <input type='radio' name='optradio' /> {t('Accrual basis')}
+                                                </label>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </Card.Header>
+                            <Card.Body style={{ paddingTop: '0px' }}>
+                                <Table responsive>
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <strong>{t('PROPERTY OR COMPANY ACCOUNT (CASH BASIS)')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('Q1')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('Q2')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('Q3')}</strong>
+                                            </th>
+                                            <th><strong>{t('TOTAL')}</strong></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>
+                                                <Link className="mr-3"> + </Link>
+                                                <Link to="/"> Garden Row (multi-building complex) - 1D | Joanne Evan</Link>
+                                            </td>
+                                            <td>
+                                                $ 3,000
+                                            </td>
+                                            <td>
+                                                $ 40,000
+                                            </td>
+                                            <td>
+                                                $ 200
+                                            </td>
+                                            <td>
+                                                $ 43,200
+                                            </td>
+                                        </tr>
+
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Fragment>
         </>
     );
-}
+};
 
 export default AnAccountingFinancials;
--- frontend/src/jsx/pages/accounting/GeneralLedger.js
+++ frontend/src/jsx/pages/accounting/GeneralLedger.js
@@ -1,15 +1,180 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PageTitle from "../../layouts/PageTitle";
+import { useState, useEffect } from "react";
+import Select from 'react-select';
+import { useTranslation } from 'react-i18next';
+import { RangeDatePicker, DatePicker } from "@y0c/react-datepicker";
+
+import {
+    Row,
+    Col,
+    Card,
+    Table,
+    Button,
+    Dropdown,
+    DropdownButton,
+    ButtonGroup,
+    SplitButton,
+} from "react-bootstrap";
+
+import '../../../css/financial.css';
+
+import { Link } from "react-router-dom";
+
+const AnAccountingGeneralLedger = () => {
+    const { t } = useTranslation();
+    const [selectedOption, setSelectedOption] = useState(null);
+    const options = [
+        { value: t('All'), label: 'All' },
+        { value: 'Multiple (55)', label: 'Multiple (55)' },
+        { value: 'Custom', label: 'Custom' },
+    ]
+    const svg1 = (
+        <svg width="20px" height="20px" viewBox="0 0 24 24" version="1.1">
+            <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                <rect x="0" y="0" width="24" height="24"></rect>
+                <circle fill="#000000" cx="5" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="12" cy="12" r="2"></circle>
+                <circle fill="#000000" cx="19" cy="12" r="2"></circle>
+            </g>
+        </svg>
+    );
+    // Normal page
+    /*  const [data, setData] = useState(
+       document.querySelectorAll(".custom-checkbox")
+     ); */
+    const [test, settest] = useState(false);
+    // use effect
+    useEffect(() => {
+        //   getAppData();
+        //setData(document.querySelectorAll(".custom-checkbox"));
+        setTimeout(() => {
+            settest(true);
+        }, 2000);
+    }, [test]);
 
 
-function AnAccountingGeneralLedger() {
     return (
         <>
-            <div className="form-head page-titles d-flex  align-items-center">
-            AnAccountingGeneralLedger            
-            </div>
+            <Fragment>
+                <div className="page-titles">
+                    <h4 className="page-title">{t('General Ledger')}</h4>
+                    <div className="property-buttons">
+                        <Button className='mr-2 btn-xs' variant='outline-dark'>{t('Record general journal entry')}</Button>
+                        <Button className='mr-2 btn-xs' variant='outline-dark'>{t('View locked periods')}</Button>
+                    </div>
+                </div>
+                <Row>
+                    <Col lg={12}>
+                        <Card>
+                            <Card.Header>
+                                <div className='col-xl-2'>
+                                    <p className="company-and-property">
+                                        {t('PROPERTY OR COMPANY')}
+                                    </p>
+                                    <Select
+                                        defaultValue={options.find(option => option.value === 'All')}
+                                        onChange={setSelectedOption}
+                                        options={options}
+                                        style={{
+                                            lineHeight: '40px',
+                                            color: '#7e7e7e',
+                                            paddingLeft: ' 15px',
+                                        }}
+                                    />
+                                </div>
+                                <div className='col-xl-2'>
+                                    <p className="company-and-property">
+                                        {t('ACCOUNTS')}
+                                    </p>
+                                    <Select
+                                        defaultValue={options.find(option => option.value === 'Multiple (55)')}
+                                        onChange={setSelectedOption}
+                                        options={options}
+                                        style={{
+                                            lineHeight: '10px',
+                                            color: '#7e7e7e',
+                                            paddingLeft: ' 15px',
+                                        }}
+                                    />
+                                </div>
+                                <div className='col-xl-2'>
+                                    <p className="company-and-property">
+                                        {t('DATE RANGE')}
+                                    </p>
+                                    <Select
+                                        defaultValue={options.find(option => option.value === 'Custom')}
+                                        onChange={setSelectedOption}
+                                        options={options}
+                                        style={{
+                                            lineHeight: '10px',
+                                            color: '#7e7e7e',
+                                            paddingLeft: ' 15px',
+                                        }}
+                                    />
+                                </div>
 
+                                <div>
+                                    <p className="company-and-property">{t('FROM')} ~ {('TO')}</p>
+                                    <RangeDatePicker
+                                        startText="Start"
+                                        endText="End"
+                                        startPlaceholder="Start Date"
+                                        endPlaceholder="End Date"
+                                    />
+                                </div>
+                                <div>
+                                    <p className="company-and-property ml-4">{t('ACCOUNTING BASIS')}</p>
+                                    <div className='card-body mt-2 financial-radio'>
+                                        <div className='basic-form'>
+                                            <form onSubmit={(e) => e.preventDefault()}>
+                                                <div className='form-group mb-0'>
+                                                    <label className='radio-inline mr-3'>
+                                                        <input type='radio' name='optradio' checked /> {t('Cash')}
+                                                    </label>
+                                                    <label className='radio-inline mr-3'>
+                                                        <input type='radio' name='optradio' /> {t('Accrual')}
+                                                    </label>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" className="mr-2 ml-3 btn btn-sm btn-success mt-4" id="btn-success">{t('Search')}</button>
+
+                            </Card.Header>
+                            <Card.Body style={{ paddingTop: '0px' }}>
+                                <Table responsive>
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <strong>{t('DATE (CASH BASIS)')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('PROPRTY OF COMPANY')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('NAME')}</strong>
+                                            </th>
+                                            <th>
+                                                <strong>{t('DESCRIPTION')}</strong>
+                                            </th>
+                                            <th></th>
+                                            <th><strong>{t('AMOUNT')}</strong></th>
+                                            <th><strong>{t('BALANCE')}</strong></th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </Table>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Fragment>
         </>
     );
-}
+};
 
 export default AnAccountingGeneralLedger;
