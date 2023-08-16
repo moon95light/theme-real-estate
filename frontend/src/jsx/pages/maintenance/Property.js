f3793fbc6295839ab2672a92bf94674abbd5fa43
antonius95 <antonio.torres.tech@gmail.com>
2023-08-16 07:27:31 +0200

    ua
    ua

--- frontend/src/jsx/pages/maintenance/Property.js
+++ frontend/src/jsx/pages/maintenance/Property.js
@@ -1,15 +1,65 @@
-import React from "react";
+import React, { Fragment } from "react";
+import PageTitle from "../../layouts/PageTitle";
+import { useState, useEffect } from "react";
+import Select from 'react-select';
+import { useTranslation } from 'react-i18next';
+import {
+    Row,
+    Col,
+    Card,
+    Table,
+    Button,
+    Dropdown,
+    DropdownButton,
+    ButtonGroup,
+    SplitButton,
+} from "react-bootstrap";
+
+import '../../../css/properties.css';
+
+import { Link } from "react-router-dom";
+
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+]
+
+const AnMaintenanceProperty = () => {
+    const { t } = useTranslation();
 
 
-function AnMaintenanceProperty() {
+    // Normal page
+    /*  const [data, setData] = useState(
+       document.querySelectorAll(".custom-checkbox")
+     ); */
+    const [test, settest] = useState(false);
+    // use effect
+    useEffect(() => {
+        //   getAppData();
+        //setData(document.querySelectorAll(".custom-checkbox"));
+        setTimeout(() => {
+            settest(true);
+        }, 2000);
+    }, [test]);
+
+
     return (
         <>
-            <div className="form-head page-titles d-flex  align-items-center">
-            AnMaintenanceProperty            
-            </div>
+            <Fragment>
+                <div className="page-titles">
+                    <h4 className="page-title">{t('Property Inspection')}</h4>
+                    <div className="property-buttons">
+                        <button type="button" className="mr-2 btn btn-xs btn-success" id="btn-success">{t('Sign up')}</button>
 
+
+                    </div>
+                </div>
+
+            </Fragment>
         </>
     );
-}
+};
+
 
 export default AnMaintenanceProperty;
